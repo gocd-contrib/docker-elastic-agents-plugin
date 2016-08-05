dbbfd907511a8b319256ca1a44ebe83fde3ae152
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-08-05 20:25:26 +0530

    Set the `AGENT_WORK_DIR`

--- docker-files/go-agent-prod.sh
+++ docker-files/go-agent-prod.sh
@@ -6,4 +6,4 @@ mkdir -p config
 echo "${AUTO_REGISTER_CONTENTS}" > config/autoregister.properties
 unset AUTO_REGISTER_CONTENTS
 
-PRODUCTION_MODE=Y /usr/share/go-agent/agent.sh
+PRODUCTION_MODE=Y AGENT_WORK_DIR=/var/lib/go-agent bash -x /usr/share/go-agent/agent.sh
