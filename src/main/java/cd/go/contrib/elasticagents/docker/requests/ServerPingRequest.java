6f12620b1181579ac4cc8d9ae1b685d024fcf116
Dhanashri Pagar <dhanashrinp3009@gmail.com>
2019-09-06 14:41:26 +0530

    Fixed typo in the Server Ping request

--- src/main/java/cd/go/contrib/elasticagents/docker/requests/ServerPingRequest.java
+++ src/main/java/cd/go/contrib/elasticagents/docker/requests/ServerPingRequest.java
@@ -27,23 +27,24 @@ import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ServerPingRequest {
     private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-    private List<ClusterProfileProperties> allClusterProfilePropertiesProperties = new ArrayList<>();
+    private List<ClusterProfileProperties> allClusterProfileProperties = new ArrayList<>();
 
     public ServerPingRequest() {
     }
 
     public ServerPingRequest(List<Map<String, String>> allClusterProfileProperties) {
-        this.allClusterProfilePropertiesProperties = allClusterProfileProperties.stream()
-                .map(clusterProfile -> ClusterProfileProperties.fromConfiguration(clusterProfile))
+        this.allClusterProfileProperties = allClusterProfileProperties.stream()
+                .map(ClusterProfileProperties::fromConfiguration)
                 .collect(Collectors.toList());
     }
 
     public List<ClusterProfileProperties> allClusterProfileProperties() {
-        return allClusterProfilePropertiesProperties;
+        return allClusterProfileProperties;
     }
 
     public static ServerPingRequest fromJSON(String json) {
@@ -53,8 +54,21 @@ public class ServerPingRequest {
     @Override
     public String toString() {
         return "ServerPingRequest{" +
-                "allClusterProfilePropertiesProperties=" + allClusterProfilePropertiesProperties +
+                "allClusterProfileProperties=" + allClusterProfileProperties +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServerPingRequest that = (ServerPingRequest) o;
+        return Objects.equals(allClusterProfileProperties, that.allClusterProfileProperties);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(allClusterProfileProperties);
     }
 
     public ServerPingRequestExecutor executor(Map<String, DockerContainers> clusterSpecificAgentInstances, PluginRequest pluginRequest) {
---
+++ src/test/java/cd/go/contrib/elasticagents/docker/requests/ServerPingRequestTest.java
@@ -0,0 +1,41 @@
+package cd.go.contrib.elasticagents.docker.requests;
+
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ServerPingRequestTest {
+    @Test
+    public void shouldDeserializeJSONBody() {
+        String requestBody = "{\n " +
+                " \"all_cluster_profile_properties\": [\n    " +
+                "{\n      " +
+                "      \"go_server_url\": \"foo\",\n" +
+                "      \"max_docker_containers\": \"100\",\n" +
+                "      \"docker_uri\": \"dockerURI\",\n" +
+                "      \"auto_register_timeout\": \"1\",\n" +
+                "      \"private_registry_password\": \"foobar\",\n" +
+                "      \"enable_private_registry_authentication\": \"false\",\n" +
+                "      \"private_registry_custom_credentials\": \"true\",\n" +
+                "      \"pull_on_container_create\": \"false\"\n" +
+                "    }\n" +
+                "   ]" +
+                "\n}";
+
+        HashMap<String, String> configurations = new HashMap<>();
+        configurations.put("go_server_url", "foo");
+        configurations.put("max_docker_containers", "100");
+        configurations.put("docker_uri", "dockerURI");
+        configurations.put("auto_register_timeout", "1");
+        configurations.put("private_registry_password", "foobar");
+        configurations.put("enable_private_registry_authentication", "false");
+        configurations.put("private_registry_custom_credentials", "true");
+        configurations.put("pull_on_container_create", "false");
+        assertThat(ServerPingRequest.fromJSON(requestBody), is(new ServerPingRequest(Arrays.asList(configurations))));
+    }
+}
